04fea5abd954bd04836088b6a421a05fef910bd1
Karl Fischer <fishi0x01@gmail.com>
2020-02-13 17:41:43 +0100

    Debian package scanner

--- src/report/rakn.rs
+++ src/report/rakn.rs
@@ -1,10 +1,12 @@
 use std::collections::HashMap;
-use crate::common::package::LibPackage;
+use crate::common::package::{LibPackage, BinaryPackage, SourcePackage};
 use crate::scanner::osinfo::OSInfoScanner;
 use crate::common::report::ReportExt;
 
 pub struct RaknReport {
     os_info: OSInfoScanner,
+    os_packages: Vec<BinaryPackage>,
+    source_packages: Vec<SourcePackage>,
     lib_packages: HashMap<String, Vec<LibPackage>>,
 }
 
@@ -16,8 +18,14 @@ impl ReportExt for RaknReport {
         report = [report, "OS Release:\t".to_string(), self.os_info.get_os_release_version(), "\n".to_string()].join(" ");
         report = [report, "Kernel:\t\t".to_string(), self.os_info.get_kernel(), "\n\n".to_string()].join(" ");
 
+        report = [report, "Installed OS Packages".to_string(), "\n".to_string()].join(" ");
+        for os_package in self.os_packages.iter() {
+            report = [report, "--".to_string(), os_package.get_name(), ":".to_string(), os_package.get_version(), "\n".to_string()].join(" ");
+        }
+
+        report = [report, "\nInstalled libraries".to_string(), "\n".to_string()].join(" ");
         for (path, packages) in self.lib_packages.iter() {
-            report = [report, "path:".to_string(), String::from(path), "\n".to_string()].join(" ");
+            report = [report, "\n".to_string(), "path:".to_string(), String::from(path), "\n".to_string()].join(" ");
             for package in packages.iter() {
                 report = [report, "--".to_string(), package.get_name(), ":".to_string(), package.get_version(), "\n".to_string()].join(" ");
             }
@@ -28,9 +36,14 @@ impl ReportExt for RaknReport {
 }
 
 impl RaknReport {
-    pub fn new(os_info: OSInfoScanner, lib_packages: HashMap<String, Vec<LibPackage>>) -> RaknReport {
+    pub fn new(os_info: OSInfoScanner,
+               os_packages: Vec<BinaryPackage>,
+               source_packages: Vec<SourcePackage>,
+               lib_packages: HashMap<String, Vec<LibPackage>>) -> RaknReport {
         RaknReport {
             os_info,
+            os_packages,
+            source_packages,
             lib_packages,
         }
     }
