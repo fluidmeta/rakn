13cb4926bb6cb05b27cc6faad613e296574ec98c
Karl Fischer <fishi0x01@gmail.com>
2020-02-15 13:08:51 +0100

    parse rpm packages

--- src/scanner/rpm.rs
+++ src/scanner/rpm.rs
@@ -1,4 +1,4 @@
-use crate::common::package::{BinaryPackage, SourcePackage};
+use crate::common::package::{BinaryPackage, BinaryPackageBuilder, SourcePackage};
 use std::process::Command;
 
 pub struct RpmScanner {}
@@ -9,12 +9,28 @@ impl RpmScanner {
     }
 
     pub fn run(self) -> (Vec<BinaryPackage>, Vec<SourcePackage>) {
+        let mut binary_packages:Vec<BinaryPackage> = vec![];
         let output = Command::new("rpm")
-            .args(&["-qa", "--queryformat", "%{NAME} %{EPOCHNUM} %{VERSION} %{RELEASE} %{ARCH}"])
+            .args(&["-qa", "--queryformat", "%{NAME} , %{EPOCHNUM} , %{VERSION} , %{RELEASE} , %{ARCH}\n"])
             .output()
             .expect("failed to execute process");
         let output_string = String::from_utf8_lossy(&output.stdout);
-        println!{"{}", output_string};
-        (vec![], vec![])
+        for line in output_string.to_string().lines().into_iter() {
+            let line_split = line.split(',').collect::<Vec<_>>();
+            if line_split.len() == 5 {
+                let v = line_split.iter().map(|s| String::from(*s))
+                    .collect::<Vec<_>>();
+                let (name, epoch_num, version, release, arch) = (
+                    v[0].as_str(), v[1].as_str(), v[2].as_str(), v[3].as_str(), v[4].as_str());
+                binary_packages.push(
+                    BinaryPackageBuilder::default()
+                        .name(String::from(name.trim()))
+                        .version(String::from(version.trim()))
+                        .build()
+                        .unwrap()
+                );
+            }
+        }
+        (binary_packages, vec![])
     }
 }
